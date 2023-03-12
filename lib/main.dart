64381cd5da6c9bd283147bfdadf51ff50137662c
Akila Mendis <akila@oneit.com.au>
2023-03-12 18:12:59 +0530

    015 Adding Layout Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,23 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What`s your favorite color?",
+      "What's your favorite animal?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text("The question !"),
+            ElevatedButton(onPressed: null, child: Text('Answer 1')),
+            ElevatedButton(onPressed: null, child: Text('Answer 2')),
+            ElevatedButton(onPressed: null, child: Text('Answer 3')),
+          ],
+        ),
       ),
     );
   }
