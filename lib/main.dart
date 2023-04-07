4e9bcb61e139ae345e3c243e3f1f889e33db78ea
Akila Mendis <akila@oneit.com.au>
2023-04-07 10:46:45 +0530

    039 Getters & else-if

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
